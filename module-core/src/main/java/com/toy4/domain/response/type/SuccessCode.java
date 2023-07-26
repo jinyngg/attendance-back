147f3596c3758290356b2dbf0e4212a5fd32a16e
jyjang <jinyngg@gmail.com>
2023-07-26 17:24:27 +0900

    feat: 공통 응답 처리 기능 개선 (#5)
    1. 성공시 Message를 직접 다룰 수 있도록 수정

--- module-core/src/main/java/com/toy4/domain/response/type/SuccessCode.java
+++ module-core/src/main/java/com/toy4/domain/response/type/SuccessCode.java
@@ -7,7 +7,13 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum SuccessCode {
 
-    SUCCESS("성공했습니다.")
+    SUCCESS("성공했습니다."),
+
+    AVAILABLE_EMAIL("사용 가능한 이메일입니다."),
+    AVAILABLE_PASSWORD("사용 가능한 패스워드입니다."),
+
+    COMPLETE_CHANGE_PASSWORD("비밀번호가 변경되었습니다."),
+    COMPLETE_EMAIL_TRANSMISSION("이메일 전송이 완료되었습니다."),
     ;
 
     private final String message;
