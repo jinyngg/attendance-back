f51957070d10cdf6480d58ef6b2e071852a08602
yeeun <dpdmstjs@gmail.com>
2023-08-01 00:07:24 +0900

    feat: separator를 사용하여 파일 경로를 반환하는 메서드 구현 (#14)
    - 운영체제에 따라 파일 경로 구분자를 반환하는 `File.separator`를 사용하여 운영체제에 맞는 파일 업로드 경로를 반환 시킴 (Windows : 백슬래시(\), Linux : 슬래시(/)가 파일 경로 구분자로 사용)

--- module-domain/src/main/java/com/toy4/global/config/FileUploadConfig.java
+++ module-domain/src/main/java/com/toy4/global/config/FileUploadConfig.java
@@ -1,5 +1,7 @@
 package com.toy4.global.config;
 
+import java.io.File;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,5 +14,8 @@ public class FileUploadConfig {
 	@Value("${file.upload.path.linux}")
 	private String fileUploadPathLinux;
 
-
+	public String getFileUploadPath() {
+		String separator = File.separator;
+		return separator.equals("\\") ? fileUploadPathWindows : fileUploadPathLinux;
+	}
 }
