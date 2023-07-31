1d13e16e34d84697860f24eea2863909e840295a
yeeun <dpdmstjs@gmail.com>
2023-08-01 00:11:23 +0900

    chore: 운영체제에 따른 파일 업로드 경로 변경 (#14)

--- module-client/src/main/java/com/toy4/global/config/WebMvcConfig.java
+++ module-client/src/main/java/com/toy4/global/config/WebMvcConfig.java
@@ -1,18 +1,23 @@
 package com.toy4.global.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.toy4.global.file.EmployeeProfileImageService;
+import lombok.AllArgsConstructor;
 
+@AllArgsConstructor
+@EnableWebMvc
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+	private final FileUploadConfig fileUploadConfig;
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry
 			.addResourceHandler("/images/**")
-			.addResourceLocations("file:///" + EmployeeProfileImageService.FILE_UPLOAD_PATH);
+			.addResourceLocations("file:///" + fileUploadConfig.getFileUploadPath());
 	}
 }
