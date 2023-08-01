10cd52f7a98fcbff3e00faa4265db4fafeac7e26
yeeun <dpdmstjs@gmail.com>
2023-08-02 00:50:54 +0900

    feat: updateEmployeeInfo 메서드 구현 (#14)
    - profileImagePath를 default 프로필 이미지 경로로 설정했습니다.
    - profile이 (required=false)로 선택사항이기 때문에 file이 null로 들어올 경우 기본 default 프로필 이미지가 들어가게 설정하였으며,file이 있다면 새로운 경로로 이미지를 저장할 수 있도록 하였습니다.

--- module-domain/src/main/java/com/toy4/domain/employee/service/EmployeeService.java
+++ module-domain/src/main/java/com/toy4/domain/employee/service/EmployeeService.java
@@ -1,11 +1,15 @@
 package com.toy4.domain.employee.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import com.toy4.domain.employee.dto.EmployeeDto;
 import com.toy4.global.response.dto.CommonResponse;
 
 public interface EmployeeService {
 
     // 이메일 유효성 확인
     CommonResponse<?> validateEmail(String email);
+    CommonResponse<?> updateEmployeeInfo(EmployeeDto employeeDto, MultipartFile file);
     CommonResponse<?> getMyPage(Long id);
 
 }
--- module-domain/src/main/java/com/toy4/domain/employee/service/impl/EmployeeServiceImpl.java
+++ module-domain/src/main/java/com/toy4/domain/employee/service/impl/EmployeeServiceImpl.java
@@ -5,12 +5,15 @@ import static com.toy4.global.response.type.SuccessCode.*;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.toy4.domain.employee.domain.Employee;
+import com.toy4.domain.employee.dto.EmployeeDto;
 import com.toy4.domain.employee.dto.MyPageResponse;
 import com.toy4.domain.employee.exception.EmployeeException;
 import com.toy4.domain.employee.repository.EmployeeRepository;
 import com.toy4.domain.employee.service.EmployeeService;
+import com.toy4.global.file.EmployeeProfileImageService;
 import com.toy4.global.response.dto.CommonResponse;
 import com.toy4.global.response.service.ResponseService;
 import com.toy4.global.response.type.ErrorCode;
@@ -23,6 +26,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 
 	private final EmployeeRepository employeeRepository;
 	private final ResponseService responseService;
+	private final EmployeeProfileImageService employeeProfileImageService;
 
 	@Override
 	public CommonResponse<?> validateEmail(String email) {
@@ -40,7 +44,25 @@ public class EmployeeServiceImpl implements EmployeeService {
 	}
 
 	@Override
-	@Transactional(readOnly = true)
+	@Transactional
+	public CommonResponse<?> updateEmployeeInfo(EmployeeDto employeeDto, MultipartFile profileImageFile) {
+		Employee employee = employeeRepository.findById(employeeDto.getId())
+			.orElseThrow(() -> new EmployeeException(ErrorCode.ENTITY_NOT_FOUND));
+
+		String profileImagePath = employeeProfileImageService.getDefaultFile();
+
+		if (!profileImageFile.isEmpty()) {
+			profileImagePath = employeeProfileImageService.saveFile(employeeDto.getId(), profileImageFile);
+        }
+
+		employeeProfileImageService.removeIfFileExists(employee.getProfilePath());
+
+		employee.update(employeeDto, profileImagePath);
+
+		return responseService.success(employee.getId(), COMPLETE_PERSONAL_INFO_UPDATE);
+	}
+
+	@Override
 	public CommonResponse<?> getMyPage(Long id) {
 		Employee employee = employeeRepository.findById(id)
 			.orElseThrow(() -> new EmployeeException(ErrorCode.ENTITY_NOT_FOUND));
