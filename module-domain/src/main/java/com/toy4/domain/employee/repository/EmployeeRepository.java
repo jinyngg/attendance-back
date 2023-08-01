3a0123274417babf95952d1bf05fa49c950b2cca
yeeun <dpdmstjs@gmail.com>
2023-08-01 23:50:16 +0900

    feat: EmployeeRepository 생성 후 findById 메서드 추가 (#14)

--- module-domain/src/main/java/com/toy4/domain/employee/repository/EmployeeRepository.java
+++ module-domain/src/main/java/com/toy4/domain/employee/repository/EmployeeRepository.java
@@ -1,11 +1,14 @@
 package com.toy4.domain.employee.repository;
 
-import com.toy4.domain.employee.domain.Employee;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.toy4.domain.employee.domain.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
   // 이메일 중복 여부 확인
   boolean existsByEmail(String email);
-
+  Optional<Employee> findById(Long id);
 }
