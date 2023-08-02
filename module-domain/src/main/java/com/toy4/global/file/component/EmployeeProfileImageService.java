0f31fc2a2453141fafe95b1bc3b44ae020b62770
Joonhee Jeong <jeonggoo75@gmail.com>
2023-08-02 16:15:46 +0900

    refactor: merge EmployeeProfileImageService (#22)

--- module-domain/src/main/java/com/toy4/global/file/component/EmployeeProfileImageService.java
+++ module-domain/src/main/java/com/toy4/global/file/component/EmployeeProfileImageService.java
@@ -1,4 +1,10 @@
 package com.toy4.global.file.component;
+
+import com.toy4.global.file.config.FileUploadConfig;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -6,55 +12,55 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.UUID;
 
-import com.toy4.global.file.config.FileUploadConfig;
-import org.springframework.stereotype.Component;
-import org.springframework.web.multipart.MultipartFile;
-import lombok.RequiredArgsConstructor;
 @Component
 @RequiredArgsConstructor
 public class EmployeeProfileImageService {
-    
-    private final FileUploadConfig fileUploadConfig;
-    
-    // TODO 파일 저장을 위한 Dir 이름 고민
-    public String saveFile(MultipartFile file) {
-//        String directoryName = employeeId + "_" + System.currentTimeMillis() + "/";
-        String directoryName = System.currentTimeMillis() + "/";
-        String filePath = fileUploadConfig.getFileUploadPath() + directoryName;
-        File directory = new File(filePath);
-        if (!directory.mkdir()) {
-            return null;
-        }
-        String originalFileName = file.getOriginalFilename();
-        String uuid = UUID.randomUUID().toString();
-        String newFileName = uuid + "_" +originalFileName;
-        try {
-            Path savePath = Paths.get(filePath + newFileName);
-            file.transferTo(savePath);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-//        return "/images/profile/" + directoryName + newFileName;
-        return "/images/" + directoryName + newFileName;
-    }
-    public String getDefaultFile() {
-        return "/images/default/user.png";
-    }
-    public void removeIfFileExists(String imagePath) {
-        Path path = Paths.get(fileUploadConfig.getFileUploadPath() + imagePath.replace("/images/profile", ""));
-        if (Files.exists(path)) {
-            removeFile(path);
-            path = path.getParent();
-            if (Files.exists(path)) {
-                removeFile(path);
-            }
-        }
-    }
-    private void removeFile(Path path) {
-        try {
-            Files.delete(path);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+
+	private final FileUploadConfig fileUploadConfig;
+
+	public String saveFile(MultipartFile file) {
+		String directoryName = System.currentTimeMillis() + "/";
+		String filePath = fileUploadConfig.getFileUploadPath() + directoryName;
+		File directory = new File(filePath);
+		if (!directory.mkdir()) {
+			return null;
+		}
+
+		String originalFileName = file.getOriginalFilename();
+		String uuid = UUID.randomUUID().toString();
+		String newFileName = uuid + "_" +originalFileName;
+
+		try {
+			Path savePath = Paths.get(filePath + newFileName);
+			file.transferTo(savePath);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+		return "/images/" + directoryName + newFileName;
+	}
+
+	public String getDefaultFile() {
+		return "/user.png";
+	}
+
+	public void removeIfFileExists(String imagePath) {
+		Path path = Paths.get(fileUploadConfig.getFileUploadPath() + imagePath.replace("/images/profile", ""));
+		if (Files.exists(path)) {
+			removeFile(path);
+
+			path = path.getParent();
+			if (Files.exists(path)) {
+				removeFile(path);
+			}
+		}
+	}
+
+	private void removeFile(Path path) {
+		try {
+			Files.delete(path);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
