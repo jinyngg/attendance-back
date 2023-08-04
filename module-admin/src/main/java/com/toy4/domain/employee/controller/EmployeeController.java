a994ab0495df8e118a465f23640b4ad00c6da576
yeeun <dpdmstjs@gmail.com>
2023-08-05 01:52:43 +0900

    feat : EmployeeController 생성 후 직원 관리, 상세, 수정에 대한 메서드 구현 (#45)

---
+++ module-admin/src/main/java/com/toy4/domain/employee/controller/EmployeeController.java
@@ -0,0 +1,63 @@
+package com.toy4.domain.employee.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.toy4.domain.dayOffHistory.service.DayOffHistoryService;
+import com.toy4.domain.dutyHistory.service.DutyHistoryService;
+import com.toy4.domain.employee.dto.EmployeeInfoRequest;
+import com.toy4.domain.employee.service.EmployeeService;
+import com.toy4.global.response.dto.CommonResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RequestMapping("api/admin")
+@RestController
+public class EmployeeController {
+
+	private final EmployeeService employeeService;
+	private final DayOffHistoryService dayOffHistoryService;
+	private final DutyHistoryService dutyHistoryService;
+
+	@GetMapping("/employees")
+	public ResponseEntity<?> getEmployees() {
+		CommonResponse<?> response = employeeService.getEmployeeDayOffInfo();
+		return ResponseEntity.ok(response);
+	}
+
+	@GetMapping(path = "/employee/{id}")
+	public ResponseEntity<?> getEmployeeInfo(@PathVariable Long id) {
+		CommonResponse<?> response = employeeService.getEmployeeInfo(id);
+		return ResponseEntity.ok(response);
+	}
+
+	@PutMapping(path="/employee",consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+	public ResponseEntity<?> updateEmployeeInfo (
+		@RequestPart EmployeeInfoRequest employeeInfoRequest,
+		@RequestPart(required=false) MultipartFile profile) {
+
+		CommonResponse<?> response = employeeService.updateEmployeeInfo(EmployeeInfoRequest.to(employeeInfoRequest), profile);
+		return ResponseEntity.ok(response);
+	}
+
+
+	@GetMapping("/employees/{employeeId}/day-offs")
+	public ResponseEntity<?> getEmployeeApprovedDayOff(@PathVariable Long employeeId) {
+		CommonResponse<?> response = dayOffHistoryService.getEmployeeApprovedDayOff(employeeId);
+		return ResponseEntity.ok(response);
+	}
+
+	@GetMapping("/employees/{employeeId}/duties")
+	public ResponseEntity<?> getEmployeeApprovedDuty(@PathVariable Long employeeId) {
+		CommonResponse<?> response = dutyHistoryService.getEmployeeApprovedDuty(employeeId);
+		return ResponseEntity.ok(response);
+	}
+}
