2ec5afb6f69c3b7fc162c664a2abb511b29a769c
Joonhee Jeong <jeonggoo75@gmail.com>
2023-08-04 15:00:17 +0900

    feat: DayOffHistoryMainController#registerDayOff (#29)
    - 컨트롤러 메소드 구현
    - 예외 핸들러 구현

---
+++ module-client/src/main/java/com/toy4/domain/dayOffHistory/DayOffRegistrationRequest.java
@@ -0,0 +1,39 @@
+package com.toy4.domain.dayOffHistory;
+
+import com.toy4.domain.dayOffHistory.dto.DayOffRegistrationDto;
+import com.toy4.domain.dayoff.type.DayOffType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class DayOffRegistrationRequest {
+
+    @NotNull
+    private Long employeeId;
+    @NotNull
+    private LocalDate startDate;
+    @NotNull
+    private LocalDate endDate;
+    @NotNull
+    private String type;
+    @NotNull
+    private String reason;
+
+    public DayOffRegistrationDto toDto() {
+        return DayOffRegistrationDto.builder()
+                .employeeId(employeeId)
+                .startDate(startDate)
+                .endDate(endDate)
+                .type(DayOffType.getByTypeString(type))
+                .reason(reason)
+                .build();
+    }
+}
