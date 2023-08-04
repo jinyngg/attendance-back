760a41bfb0fac2923076da95086895d8172dddde
yeeun <dpdmstjs@gmail.com>
2023-08-04 14:09:00 +0900

    feat : 당직 승인 요청에 대한 ApprovedDutyResponse dto 생성  (#30)

---
+++ module-domain/src/main/java/com/toy4/domain/dutyHistory/dto/ApprovedDutyResponse.java
@@ -0,0 +1,24 @@
+package com.toy4.domain.dutyHistory.dto;
+
+import static com.toy4.global.date.DateFormatter.*;
+import com.toy4.domain.dutyHistory.domain.DutyHistory;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class ApprovedDutyResponse {
+	private Long dutyId;
+	private String  requestDate;
+	private String date;
+	private String status;
+
+	public static ApprovedDutyResponse from(DutyHistory dutyHistory) {
+		return ApprovedDutyResponse.builder()
+			.dutyId(dutyHistory.getId())
+			.requestDate(dutyHistory.getCreatedAt().format(formatter))
+			.date(dutyHistory.getDate().format(formatter))
+			.status(dutyHistory.getStatus().getDescription())
+			.build();
+	}
+}
