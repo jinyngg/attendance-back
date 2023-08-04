218c78d377f1aba77eb74be61febd26f46f62e6c
yeeun <dpdmstjs@gmail.com>
2023-08-04 20:22:05 +0900

    feat : dayOffHistoryRepository 추가 (#31)

--- module-domain/src/main/java/com/toy4/domain/dayOffHistory/repository/DayOffHistoryRepository.java
+++ module-domain/src/main/java/com/toy4/domain/dayOffHistory/repository/DayOffHistoryRepository.java
@@ -1,12 +1,15 @@
 package com.toy4.domain.dayOffHistory.repository;
 
 import com.toy4.domain.dayOffHistory.domain.DayOffHistory;
+import com.toy4.domain.schedule.RequestStatus;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface DayOffHistoryRepository extends JpaRepository<DayOffHistory, Long> {
@@ -14,8 +17,17 @@ public interface DayOffHistoryRepository extends JpaRepository<DayOffHistory, Lo
     List<DayOffHistory> findByEmployeeId(Long employeeId);
 
     @Query("SELECT s FROM DayOffHistory s " +
-            "WHERE s.employee.id = :employeeId " +
-            "AND s.startDate <= :endDate " +
-            "AND s.endDate >= :startDate")
+        "WHERE s.employee.id = :employeeId " +
+        "AND s.startDate <= :endDate " +
+        "AND s.endDate >= :startDate")
     List<DayOffHistory> findByEmployeeIdAndDateRange(Long employeeId, LocalDate startDate, LocalDate endDate);
+
+    @Query("SELECT s FROM DayOffHistory s " +
+        "WHERE s.employee.id = :employeeId " +
+        "AND s.status = :status")
+    List<DayOffHistory> findByEmployeeIdAndStatus(Long employeeId, RequestStatus status);
+
+    List<DayOffHistory> findAll();
+    Optional<DayOffHistory> findById(Long id);
+
 }
