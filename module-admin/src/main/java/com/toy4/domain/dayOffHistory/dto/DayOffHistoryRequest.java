dd6aaeea89cb7bf2d64ec39c72ed6d95b998aa34
yeeun <dpdmstjs@gmail.com>
2023-08-04 20:13:25 +0900

    feat : 연차 요청에 대한 dto 생성 (#31)

---
+++ module-admin/src/main/java/com/toy4/domain/dayOffHistory/dto/DayOffHistoryRequest.java
@@ -0,0 +1,28 @@
+package com.toy4.domain.dayOffHistory.dto;
+
+import javax.validation.constraints.NotBlank;
+
+import com.toy4.domain.schedule.RequestStatus;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@Getter
+@Builder
+public class DayOffHistoryRequest {
+	private Long dayOffId;
+	@NotBlank(message = "상태가 누락되었습니다.")
+	private String status;
+
+	public static DayOffHistoryDto to(DayOffHistoryRequest request) {
+		return DayOffHistoryDto.builder()
+			.id(request.getDayOffId())
+			.status(RequestStatus.getByTypeString(request.getStatus()))
+			.build();
+	}
+}
