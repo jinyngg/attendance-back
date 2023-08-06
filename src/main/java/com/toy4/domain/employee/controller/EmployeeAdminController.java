ee2e60144b042c6953be4f00a71c203b9394d623
Joonhee Jeong <jeonggoo75@gmail.com>
2023-08-06 22:45:47 +0900

    refactor: rename EmployeeController (#50)
    - 의미를 명확하게 하여 혼동을 피하기 위하여 EmployeeAdminController 로 수정

--- src/main/java/com/toy4/domain/employee/controller/EmployeeAdminController.java
+++ src/main/java/com/toy4/domain/employee/controller/EmployeeAdminController.java
@@ -14,7 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 @RequiredArgsConstructor
 @RequestMapping("/api/admin")
 @RestController
-public class EmployeeController {
+public class EmployeeAdminController {
 
 	private final EmployeeService employeeService;
 	private final DayOffHistoryService dayOffHistoryService;
