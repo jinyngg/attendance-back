d59ac1d90dce5c50bcb54ddc6c65fff5160598aa
Joonhee Jeong <jeonggoo75@gmail.com>
2023-08-08 17:58:53 +0900

    feat: 날짜가 겹치는 연차 조회 (#66)
    - 반차인 경우 연차나 같은 타입의 반차에서 조회
    - 연차인 경우 모든 연차 유형에서 조회

--- src/main/java/com/toy4/domain/dayOffHistory/repository/DayOffHistoryRepository.java
+++ src/main/java/com/toy4/domain/dayOffHistory/repository/DayOffHistoryRepository.java
@@ -1,8 +1,9 @@
 package com.toy4.domain.dayOffHistory.repository;
 
 import com.toy4.domain.dayOffHistory.domain.DayOffHistory;
+import com.toy4.domain.dayoff.type.DayOffType;
+import com.toy4.domain.employee.domain.Employee;
 import com.toy4.domain.schedule.RequestStatus;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -27,7 +28,22 @@ public interface DayOffHistoryRepository extends JpaRepository<DayOffHistory, Lo
     @Query("SELECT s FROM DayOffHistory  s " +
         "WHERE s.employee.id = :employeeId " +
         "AND :date BETWEEN s.startDate AND s.endDate " +
-        "AND s.status IN ( :#{T(com.toy4.domain.schedule.RequestStatus).REQUESTED}, :#{T(com.toy4.domain.schedule.RequestStatus).APPROVED} )" +
-        "AND s.dayOff.type IN ( :#{T(com.toy4.domain.dayoff.type.DayOffType).NORMAL_DAY_OFF}, :#{T(com.toy4.domain.dayoff.type.DayOffType).SPECIAL_DAY_OFF})")
+        "AND s.status IN ( :#{T(com.toy4.domain.schedule.RequestStatus).REQUESTED}, :#{T(com.toy4.domain.schedule.RequestStatus).APPROVED} ) " +
+        "AND s.dayOff.type IN ( :#{T(com.toy4.domain.dayoff.type.DayOffType).NORMAL_DAY_OFF}, :#{T(com.toy4.domain.dayoff.type.DayOffType).SPECIAL_DAY_OFF} )")
     Optional<DayOffHistory> findOverlappedDate(Long employeeId, LocalDate date);
+
+    @Query("SELECT s FROM DayOffHistory  s " +
+           "WHERE s.employee = :employee " +
+             "AND s.status IN ( :#{T(com.toy4.domain.schedule.RequestStatus).REQUESTED}, :#{T(com.toy4.domain.schedule.RequestStatus).APPROVED} ) " +
+             "AND :date BETWEEN s.startDate AND s.endDate " +
+             "AND s.dayOff.type IN ( :type, :#{T(com.toy4.domain.dayoff.type.DayOffType).NORMAL_DAY_OFF}, :#{T(com.toy4.domain.dayoff.type.DayOffType).SPECIAL_DAY_OFF} ) ")
+    List<DayOffHistory> findAllOverlappedDate(Employee employee, DayOffType type, LocalDate date);
+
+    @Query("SELECT s FROM DayOffHistory  s " +
+           "WHERE s.employee = :employee " +
+             "AND s.status IN ( :#{T(com.toy4.domain.schedule.RequestStatus).REQUESTED}, :#{T(com.toy4.domain.schedule.RequestStatus).APPROVED} ) " +
+             "AND ( ( s.startDate BETWEEN :startDate AND :endDate ) " +
+             "  OR ( s.endDate BETWEEN :startDate AND :endDate ) " +
+             "  OR ( s.startDate <= :startDate AND :endDate <= s.endDate ) )")
+    List<DayOffHistory> findAllOverlappedDate(Employee employee, LocalDate startDate, LocalDate endDate);
 }
