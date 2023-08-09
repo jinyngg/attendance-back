edcb2d9270bc0251a2281a0d2ee18c9545fa5d82
yeeun <dpdmstjs@gmail.com>
2023-08-09 23:54:14 +0900

    refactor : employeeInfo dto 정의 (#88)

---
+++ src/main/java/com/toy4/domain/employee/dto/response/EmployeeInfo.java
@@ -0,0 +1,24 @@
+package com.toy4.domain.employee.dto.response;
+
+import java.time.LocalDate;
+
+import com.toy4.domain.department.type.DepartmentType;
+import com.toy4.domain.position.type.PositionType;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class EmployeeInfo {
+
+	private Long employeeId;
+	private DepartmentType departmentType;
+	private PositionType positionType;
+	private String name;
+	private String phone;
+	private LocalDate hireDate;
+
+}
