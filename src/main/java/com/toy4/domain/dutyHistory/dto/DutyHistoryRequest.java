6fe1faedcebc9d09be0acd917128eb565a355c71
Joonhee Jeong <jeonggoo75@gmail.com>
2023-08-09 22:17:40 +0900

    refactor: modify validation in DutyHistoryRequest (#90)

--- src/main/java/com/toy4/domain/dutyHistory/dto/DutyHistoryRequest.java
+++ src/main/java/com/toy4/domain/dutyHistory/dto/DutyHistoryRequest.java
@@ -1,24 +1,17 @@
 package com.toy4.domain.dutyHistory.dto;
+
+import com.toy4.domain.schedule.RequestStatus;
+import lombok.Getter;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
-import com.toy4.domain.schedule.RequestStatus;
-
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-@AllArgsConstructor
 @Getter
-@Builder
 public class DutyHistoryRequest {
 
 	@NotNull
 	private Long dutyId;
-	@NotBlank(message = "상태가 누락되었습니다.")
+	@NotBlank
 	private String status;
 
 	public static DutyHistoryDto to(DutyHistoryRequest request) {
