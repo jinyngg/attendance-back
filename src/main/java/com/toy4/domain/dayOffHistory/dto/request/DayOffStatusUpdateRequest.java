8ac0a52b5ed0066a893a7b4b3e62b6c2b5dbea89
Joonhee Jeong <jeonggoo75@gmail.com>
2023-08-10 15:15:45 +0900

    refactor: 불필요 어노테이션 정리 (#90)

--- src/main/java/com/toy4/domain/dayOffHistory/dto/request/DayOffStatusUpdateRequest.java
+++ src/main/java/com/toy4/domain/dayOffHistory/dto/request/DayOffStatusUpdateRequest.java
@@ -2,20 +2,17 @@ package com.toy4.domain.dayOffHistory.dto.request;
 
 import com.toy4.domain.dayOffHistory.dto.DayOffStatusUpdate;
 import com.toy4.domain.schedule.RequestStatus;
-import lombok.*;
+import lombok.Getter;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-@AllArgsConstructor
 @Getter
-@Builder
 public class DayOffStatusUpdateRequest {
 
 	@NotNull
 	private Long dayOffId;
-	@NotBlank(message = "상태가 누락되었습니다.")
+	@NotBlank
 	private String status;
 
 	public DayOffStatusUpdate toDto() {
