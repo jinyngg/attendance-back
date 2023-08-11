7126a04f959e7481fe2b60357520dd2fec746dc6
Joonhee Jeong <jeonggoo75@gmail.com>
2023-08-11 15:37:52 +0900

    refactor: 어드민 더미데이터 추가, 더미데이터 패스워드 인코딩 (#115)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -48,22 +48,22 @@ VALUES
 -- 직원
 INSERT INTO employee (auth_token, position_id, department_id, status_id, name, email, password, hire_date, day_off_remains, role, phone, birthdate)
 VALUES
-    ('b902fc85-ccc5-4b5e-9a79-6488db8827bf', 1, 1, 1, '테스트유저1', 'testuser1@email.com', 'test1234', '2023-03-02', 11, 'USER', '010-1234-5712', '1996-07-30'),
-    ('b902fc85-ccc5-4b5e-9a79-7488db8827bf', 1, 2, 1, '테스트유저2', 'testuser2@email.com', 'test1234', '2023-03-02', 11.5, 'USER', '010-1234-5711', '1998-01-31'),
-    ('b902fc85-ccc5-4b5e-9a79-8488db8827bf', 1, 3, 1, '테스트유저3', 'testuser3@email.com', 'test1234', '2023-03-02', 11, 'USER', '010-1234-5710', '1995-04-13'),
-    ('b902fc85-ccc5-4b5e-9a79-9488db8827bf', 1, 4, 1, '테스트유저4', 'testuser4@email.com', 'test1234', '2022-03-02', 11.5, 'USER', '010-1234-5709', '1994-07-25'),
-    ('b902fc85-ccc5-4b5e-9a79-6088db8827bf', 1, 5, 1, '테스트유저5', 'testuser5@email.com', 'test1234', '2022-03-02', 11.5, 'USER', '010-1234-5708', '1996-08-14'),
-    ('b902fc85-ccc5-4b5e-9a79-6188db8827bf', 2, 6, 1, '테스트유저6', 'testuser6@email.com', 'test1234', '2020-02-28', 14, 'USER', '010-1234-5707', '1990-06-30'),
-    ('b902fc85-ccc5-4b5e-9a79-6288db8827bf', 2, 1, 1, '테스트유저7', 'testuser7@email.com', 'test1234', '2020-02-28', 16.5, 'USER', '010-1234-5706', '1992-09-15'),
-    ('b902fc85-ccc5-4b5e-9a79-6388db8827bf', 2, 2, 1, '테스트유저8', 'testuser8@email.com', 'test1234', '2018-07-23', 14, 'USER', '010-1234-5705', '1989-12-31'),
-    ('b902fc85-ccc5-4b5e-9a79-7088db8827bf', 2, 3, 1, '테스트유저9', 'testuser9@email.com', 'test1234', '2018-07-23', 16.5, 'USER', '010-1234-5704', '1988-07-29'),
-    ('b902fc85-ccc5-4b5e-9a79-7188db8827bf', 3, 4, 1, '테스트유저10', 'testuser10@email.com', 'test1234', '2017-03-02', 19, 'USER', '010-1234-5704', '1985-08-01'),
-    ('b902fc85-ccc5-4b5e-9a79-7288db8827bf', 3, 5, 1, '테스트유저11', 'testuser11@email.com', 'test1234', '2017-03-02', 19, 'USER', '010-1234-5703', '1991-11-30'),
-    ('b902fc85-ccc5-4b5e-9a79-7388db8827bf', 3, 6, 1, '테스트유저12', 'testuser12@email.com', 'test1234', '2017-03-02', 19, 'USER', '010-1234-5702', '1983-10-11'),
-    ('b902fc85-ccc5-4b5e-9a79-8088db8827bf', 4, 1, 1, '테스트유저13', 'testuser13@email.com', 'test1234', '2016-09-02', 21, 'USER', '010-1234-5701', '1970-07-30'),
-    ('b902fc85-ccc5-4b5e-9a79-8188db8827bf', 4, 2, 1, '테스트유저14', 'testuser14@email.com', 'test1234', '2016-09-02', 21, 'USER', '010-1234-5700', '1968-02-09'),
-    ('b902fc85-ccc5-4b5e-9a79-8288db8827bf', 5, 4, 1, '테스트유저15', 'testuser15@email.com', 'test1234', '2015-03-02', 23, 'USER', '010-1234-5679', '1960-03-29'),
-    ('b902fc85-ccc5-4b5e-9a79-9988db8827bf', 5, 6, 1, '테스트유저16', 'testuser16@email.com', 'test1234', '2014-12-01', 23, 'ADMIN', '010-1234-5678', '1956-02-10');
+    ('b902fc85-ccc5-4b5e-9a79-6488db8827bf', 1, 1, 1, '테스트유저01', 'testuser01@soonyang.store', '$2a$10$UgQFZScLVBRY20SfV21DG.ZhKRwVRe0wvA4tPXMQqNAaR.jTLNAAu', '2023-03-02', 11, 'USER', '010-1234-5712', '1996-07-30'),
+    ('b902fc85-ccc5-4b5e-9a79-7488db8827bf', 1, 2, 1, '테스트유저02', 'testuser02@soonyang.store', '$2a$10$UgQFZScLVBRY20SfV21DG.ZhKRwVRe0wvA4tPXMQqNAaR.jTLNAAu', '2023-03-02', 11.5, 'USER', '010-1234-5711', '1998-01-31'),
+    ('b902fc85-ccc5-4b5e-9a79-8488db8827bf', 1, 3, 1, '테스트유저03', 'testuser03@soonyang.store', '$2a$10$UgQFZScLVBRY20SfV21DG.ZhKRwVRe0wvA4tPXMQqNAaR.jTLNAAu', '2023-03-02', 11, 'USER', '010-1234-5710', '1995-04-13'),
+    ('b902fc85-ccc5-4b5e-9a79-9488db8827bf', 1, 4, 1, '테스트유저04', 'testuser04@soonyang.store', '$2a$10$UgQFZScLVBRY20SfV21DG.ZhKRwVRe0wvA4tPXMQqNAaR.jTLNAAu', '2022-03-02', 11.5, 'USER', '010-1234-5709', '1994-07-25'),
+    ('b902fc85-ccc5-4b5e-9a79-6088db8827bf', 1, 5, 1, '테스트유저05', 'testuser05@soonyang.store', '$2a$10$UgQFZScLVBRY20SfV21DG.ZhKRwVRe0wvA4tPXMQqNAaR.jTLNAAu', '2022-03-02', 11.5, 'USER', '010-1234-5708', '1996-08-14'),
+    ('b902fc85-ccc5-4b5e-9a79-6188db8827bf', 2, 6, 1, '테스트유저06', 'testuser06@soonyang.store', '$2a$10$UgQFZScLVBRY20SfV21DG.ZhKRwVRe0wvA4tPXMQqNAaR.jTLNAAu', '2020-02-28', 14, 'USER', '010-1234-5707', '1990-06-30'),
+    ('b902fc85-ccc5-4b5e-9a79-6288db8827bf', 2, 1, 1, '테스트유저07', 'testuser07@soonyang.store', '$2a$10$UgQFZScLVBRY20SfV21DG.ZhKRwVRe0wvA4tPXMQqNAaR.jTLNAAu', '2020-02-28', 16.5, 'USER', '010-1234-5706', '1992-09-15'),
+    ('b902fc85-ccc5-4b5e-9a79-6388db8827bf', 2, 2, 1, '테스트유저08', 'testuser08@soonyang.store', '$2a$10$UgQFZScLVBRY20SfV21DG.ZhKRwVRe0wvA4tPXMQqNAaR.jTLNAAu', '2018-07-23', 14, 'USER', '010-1234-5705', '1989-12-31'),
+    ('b902fc85-ccc5-4b5e-9a79-7088db8827bf', 2, 3, 1, '테스트유저09', 'testuser09@soonyang.store', '$2a$10$UgQFZScLVBRY20SfV21DG.ZhKRwVRe0wvA4tPXMQqNAaR.jTLNAAu', '2018-07-23', 16.5, 'USER', '010-1234-5704', '1988-07-29'),
+    ('b902fc85-ccc5-4b5e-9a79-7188db8827bf', 3, 4, 1, '테스트유저10', 'testuser10@soonyang.store', '$2a$10$UgQFZScLVBRY20SfV21DG.ZhKRwVRe0wvA4tPXMQqNAaR.jTLNAAu', '2017-03-02', 19, 'USER', '010-1234-5704', '1985-08-01'),
+    ('b902fc85-ccc5-4b5e-9a79-7288db8827bf', 3, 5, 1, '테스트유저11', 'testuser11@soonyang.store', '$2a$10$UgQFZScLVBRY20SfV21DG.ZhKRwVRe0wvA4tPXMQqNAaR.jTLNAAu', '2017-03-02', 19, 'USER', '010-1234-5703', '1991-11-30'),
+    ('b902fc85-ccc5-4b5e-9a79-7388db8827bf', 3, 6, 1, '테스트유저12', 'testuser12@soonyang.store', '$2a$10$UgQFZScLVBRY20SfV21DG.ZhKRwVRe0wvA4tPXMQqNAaR.jTLNAAu', '2017-03-02', 19, 'USER', '010-1234-5702', '1983-10-11'),
+    ('b902fc85-ccc5-4b5e-9a79-8088db8827bf', 4, 1, 1, '테스트유저13', 'testuser13@soonyang.store', '$2a$10$UgQFZScLVBRY20SfV21DG.ZhKRwVRe0wvA4tPXMQqNAaR.jTLNAAu', '2016-09-02', 21, 'USER', '010-1234-5701', '1970-07-30'),
+    ('b902fc85-ccc5-4b5e-9a79-8188db8827bf', 4, 2, 1, '테스트유저14', 'testuser14@soonyang.store', '$2a$10$UgQFZScLVBRY20SfV21DG.ZhKRwVRe0wvA4tPXMQqNAaR.jTLNAAu', '2016-09-02', 21, 'USER', '010-1234-5700', '1968-02-09'),
+    ('b902fc85-ccc5-4b5e-9a79-8288db8827bf', 5, 4, 1, '테스트유저15', 'testuser15@soonyang.store', '$2a$10$UgQFZScLVBRY20SfV21DG.ZhKRwVRe0wvA4tPXMQqNAaR.jTLNAAu', '2015-03-02', 23, 'USER', '010-1234-5679', '1960-03-29'),
+    ('b902fc85-ccc5-4b5e-9a79-9988db8827bf', 5, 6, 1, '어드민', 'admin@soonyang.store', '$2a$10$/OodUdtfndOpPZKaaSns2ercgvpQbtL26WX6uricymhFvrkzSNbla', '2014-12-01', 23, 'ADMIN', '010-1234-5678', '1956-02-10');
 
 INSERT INTO day_off_history(employee_id, day_off_id, status, start_date, end_date, total_amount, reason)
 VALUES
