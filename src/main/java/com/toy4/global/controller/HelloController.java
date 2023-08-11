1d6b39d60bad62222a26c96f316c21a78cfcc8e0
Joonhee Jeong <jeonggoo75@gmail.com>
2023-08-11 15:53:44 +0900

    chore: 패스워드 인코더 컨트롤러 추가

--- src/main/java/com/toy4/global/controller/HelloController.java
+++ src/main/java/com/toy4/global/controller/HelloController.java
@@ -1,16 +1,27 @@
 package com.toy4.global.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 public class HelloController {
+
+    private final PasswordEncoder passwordEncoder;
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/")
     public String hello() {
         return "Hello, FC-MINI-4 attendance service!";
+    }
+
+    @GetMapping("/{password}")
+    public String encodePassword(@PathVariable String password) {
+        return passwordEncoder.encode(password);
     }
 }
